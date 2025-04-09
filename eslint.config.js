0295cfd2ad562aa96617ef8aba32954b018b9757
Luca Pattocchio <pattocchioluca@gmail.com>
2025-04-09 20:46:41 +0200

    ref: clean up ESLint configuration comments and enhance ignores pattern

--- eslint.config.js
+++ eslint.config.js
@@ -5,17 +5,14 @@ import { FlatCompat } from '@eslint/eslintrc';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// Necessario per __dirname in moduli ES
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Crea un'istanza FlatCompat per la cartella client
 const clientCompatConfig = new FlatCompat({
   baseDirectory: path.resolve(__dirname, 'apps/client'),
 });
 
 export default defineConfig([
-  // Configurazione globale
   {
     ignores: [
       'coverage',
@@ -23,17 +20,16 @@ export default defineConfig([
       '**/dist',
       'pnpm-lock.yaml',
       'pnpm-workspace.yaml',
-      'node_modules',
+      '**/node_modules',
+      '.next',
+      '.husky',
     ],
   },
 
-  // Configurazione base TypeScript
   tseslint.configs.recommended,
 
-  // Configurazione Prettier per tutti i file
   eslintPluginPrettierRecommended,
 
-  // Configurazione specifica per Next.js
   {
     files: ['apps/client/**/*.{js,jsx,ts,tsx}'],
     ...clientCompatConfig.extends('next/core-web-vitals', 'next/typescript'),
